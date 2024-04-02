7a62de45c8d4504a58154677a24d61d3f8ef6ed1
Veljko03 <veljkodovragovic@gmail.com>
2024-04-02 21:25:51 +0200

    some setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebplackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebplackPlugin({
+        template: './src/index.html',
+        title: "Some title",
+        filename: 'index.html',
+        inject: 'body',
+    })
+  ]
+};
